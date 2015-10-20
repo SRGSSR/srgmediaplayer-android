1f0f058f80609e069502adb35b3861782fb5c916
pietrini <nico.pietri@gmail.com>
2015-10-20 09:28:13 +0200

    First implementation of Google Cast feature

--- demo/src/main/java/ch/srg/mediaplayer/DemoMediaPlayerActivity.java
+++ demo/src/main/java/ch/srg/mediaplayer/DemoMediaPlayerActivity.java
@@ -6,14 +6,36 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.MediaRouteActionProvider;
+import android.support.v7.media.MediaRouteSelector;
+import android.support.v7.media.MediaRouter;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.cast.ApplicationMetadata;
+import com.google.android.gms.cast.Cast;
+import com.google.android.gms.cast.CastDevice;
+import com.google.android.gms.cast.CastMediaControlIntent;
+import com.google.android.gms.cast.MediaInfo;
+import com.google.android.gms.cast.MediaMetadata;
+import com.google.android.gms.cast.MediaStatus;
+import com.google.android.gms.cast.RemoteMediaPlayer;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -30,6 +52,7 @@ import ch.srg.mediaplayer.demo.R;
 import ch.srg.mediaplayer.extras.dataproviders.MultiDataProvider;
 import ch.srg.mediaplayer.extras.fullscreen.helper.SystemUiHelper;
 import ch.srg.mediaplayer.extras.overlay.error.SimpleErrorMessage;
+import ch.srg.mediaplayer.internal.cast.GoogleCastDelegate;
 import ch.srg.segmentoverlay.controller.SegmentController;
 import ch.srg.segmentoverlay.model.Segment;
 import ch.srg.segmentoverlay.view.PlayerControlView;
@@ -49,6 +72,7 @@ public class DemoMediaPlayerActivity extends AppCompatActivity implements
 
     public static final String PLAYER_TAG = "main";
     private static final String TAG = "DemoSegment";
+    private static final double VOLUME_INCREMENT = 0.1;
 
 
     private SRGMediaPlayerController srgMediaPlayer;
@@ -63,6 +87,8 @@ public class DemoMediaPlayerActivity extends AppCompatActivity implements
     private LivePlayerControlView livePlayerControlView;
 
     private DemoSegmentAdapter adapter;
+
+    private Toolbar toolbar;
 
     private SegmentController segmentController;
     private TextView statusLabel;
@@ -79,6 +105,19 @@ public class DemoMediaPlayerActivity extends AppCompatActivity implements
     private int toastCount;
     private int orientation;
 
+    //Cast objects
+    private MediaRouter mMediaRouter;
+    private MediaRouteSelector mMediaRouteSelector;
+    private MyMediaRouterCallback mMediaRouterCallback;
+    private GoogleApiClient mApiClient;
+    private boolean mWaitingForReconnect;
+    private boolean mApplicationStarted;
+    private CastDevice mSelectedDevice;
+    private String mSessionId;
+    private ConnectionCallbacks mConnectionCallbacks;
+    private ConnectionFailedListener mConnectionFailedListener;
+    private RemoteMediaPlayer mRemoteMediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -89,9 +128,18 @@ public class DemoMediaPlayerActivity extends AppCompatActivity implements
 
         if (getIntent().getBooleanExtra(ARG_LIVE, false)) {
             setContentView(R.layout.activity_demo_live_media_player);
+            setTitle("DEMO Live");
         } else {
             setContentView(R.layout.activity_demo_segment_media_player);
+            setTitle("DEMO Segment");
         }
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        initGoogleCast();
+
         playerView = (SRGMediaPlayerView) findViewById(R.id.demo_video_container);
 
         errorMessage = (SimpleErrorMessage) findViewById(R.id.error_message);
@@ -111,7 +159,7 @@ public class DemoMediaPlayerActivity extends AppCompatActivity implements
                 R.id.button_seek_one_minute,
                 R.id.button_seek_live
         };
-        for (int buttonId: buttonIds) {
+        for (int buttonId : buttonIds) {
             View button = findViewById(buttonId);
             if (button != null) {
                 button.setOnClickListener(this);
@@ -197,21 +245,65 @@ public class DemoMediaPlayerActivity extends AppCompatActivity implements
         }
     }
 
+    private void initGoogleCast() {
+        mMediaRouter = MediaRouter.getInstance(getApplicationContext());
+        mMediaRouteSelector = new MediaRouteSelector.Builder()
+                .addControlCategory(CastMediaControlIntent.categoryForCast(CastMediaControlIntent.DEFAULT_MEDIA_RECEIVER_APPLICATION_ID))
+                .build();
+
+        mMediaRouterCallback = new MyMediaRouterCallback();
+        mConnectionCallbacks = new ConnectionCallbacks();
+        mConnectionFailedListener = new ConnectionFailedListener();
+
+        mRemoteMediaPlayer = new RemoteMediaPlayer();
+    }
+
     public String getTestListItem(int i) {
         return identifierList.get(i);
     }
 
     public void playTestIdentifier(String identifier) {
-        try {
-            String SEEK_DELIMITER = "@seek:";
-            if (identifier.contains(SEEK_DELIMITER)) {
-                Long time = Long.parseLong(identifier.substring(identifier.indexOf(SEEK_DELIMITER) + SEEK_DELIMITER.length())) * 1000;
-                srgMediaPlayer.play(identifier, time);
-            } else {
-                srgMediaPlayer.play(identifier);
+        if (mApiClient != null && mRemoteMediaPlayer != null && mApplicationStarted) {
+            playMediaOnGoogleCast(identifier, 0);
+        } else {
+            try {
+                String SEEK_DELIMITER = "@seek:";
+                if (identifier.contains(SEEK_DELIMITER)) {
+                    Long time = Long.parseLong(identifier.substring(identifier.indexOf(SEEK_DELIMITER) + SEEK_DELIMITER.length())) * 1000;
+                    srgMediaPlayer.play(identifier, time);
+                } else {
+                    srgMediaPlayer.play(identifier);
+                }
+            } catch (SRGMediaPlayerException e) {
+                Log.e(TAG, "play " + identifier, e);
             }
+        }
+    }
+
+    private void playMediaOnGoogleCast(String identifier, long position) {
+        String uri = null;
+        try {
+            uri = String.valueOf(dataProvider.getUri(identifier));
         } catch (SRGMediaPlayerException e) {
-            Log.e(TAG, "play " + identifier, e);
+            e.printStackTrace();
+        }
+        MediaMetadata mediaMetadata = new MediaMetadata(MediaMetadata.MEDIA_TYPE_MOVIE);
+        mediaMetadata.putString(MediaMetadata.KEY_TITLE, identifier);
+        MediaInfo mediaInfo = new MediaInfo.Builder(
+                uri)
+                .setContentType("application/vnd.apple.mpegurl")
+                .setStreamType(MediaInfo.STREAM_TYPE_BUFFERED)
+                .setMetadata(mediaMetadata)
+                .build();
+
+        if (!TextUtils.isEmpty(uri)) {
+            try {
+                mRemoteMediaPlayer.load(mApiClient, mediaInfo, true, position);
+            } catch (IllegalStateException e) {
+                Log.e(TAG, "Problem occurred with media during loading", e);
+            } catch (Exception e) {
+                Log.e(TAG, "Problem opening media during loading", e);
+            }
         }
     }
 
@@ -224,6 +316,17 @@ public class DemoMediaPlayerActivity extends AppCompatActivity implements
         if (livePlayerControlView != null) {
             livePlayerControlView.startListening();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.demo_segment_media_player_activity, menu);
+        MenuItem mediaRouteMenuItem = menu.findItem(R.id.media_route_menu_item);
+        MediaRouteActionProvider mediaRouteActionProvider =
+                (MediaRouteActionProvider) MenuItemCompat.getActionProvider(mediaRouteMenuItem);
+        mediaRouteActionProvider.setRouteSelector(mMediaRouteSelector);
+        return true;
     }
 
     private void loadIdentifiers() {
@@ -314,17 +417,15 @@ public class DemoMediaPlayerActivity extends AppCompatActivity implements
                 @NonNull URLConnectionProcessor<T> processor) {
         HttpURLConnection httpURLConnection = null;
 
-        try
-        {
+        try {
             httpURLConnection = (HttpURLConnection) new URL(url).openConnection();
 
             processor.onSetupHttpURLConnection(httpURLConnection);
-                Log.d(TAG, "Fetching " + url);
+            Log.d(TAG, "Fetching " + url);
 
             int responseCode = httpURLConnection.getResponseCode();
 
-            switch (responseCode)
-            {
+            switch (responseCode) {
                 case HttpURLConnection.HTTP_OK:
                 case HttpURLConnection.HTTP_NOT_AUTHORITATIVE:
                     InputStream inputStream = httpURLConnection.getInputStream();
@@ -336,16 +437,11 @@ public class DemoMediaPlayerActivity extends AppCompatActivity implements
                             httpURLConnection.hashCode(), responseCode, url));
                     return processor.onHttpURLConnectionError(responseCode, null);
             }
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             Log.e(TAG, "IO error for " + url + " " + e.toString());
             return processor.onHttpURLConnectionError(0, e);
-        }
-        finally
-        {
-            if (httpURLConnection != null)
-            {
+        } finally {
+            if (httpURLConnection != null) {
                 httpURLConnection.disconnect();
             }
         }
@@ -404,6 +500,13 @@ public class DemoMediaPlayerActivity extends AppCompatActivity implements
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        mMediaRouter.addCallback(mMediaRouteSelector, mMediaRouterCallback,
+                MediaRouter.CALLBACK_FLAG_REQUEST_DISCOVERY);
+    }
+
+    @Override
     protected void onPause() {
         srgMediaPlayer.unbindFromMediaPlayerView();
         segmentController.stopListening();
@@ -417,6 +520,7 @@ public class DemoMediaPlayerActivity extends AppCompatActivity implements
 
     @Override
     protected void onStop() {
+        mMediaRouter.removeCallback(mMediaRouterCallback);
         super.onStop();
     }
 
@@ -474,4 +578,190 @@ public class DemoMediaPlayerActivity extends AppCompatActivity implements
     public MultiDataProvider getDataProvider() {
         return dataProvider;
     }
+
+
+    private class MyMediaRouterCallback extends MediaRouter.Callback {
+
+        @Override
+        public void onRouteSelected(MediaRouter router, MediaRouter.RouteInfo info) {
+            mSelectedDevice = CastDevice.getFromBundle(info.getExtras());
+            String routeId = info.getId();
+
+            Log.d(TAG, routeId);
+
+            Cast.CastOptions.Builder apiOptionsBuilder = Cast.CastOptions
+                    .builder(mSelectedDevice, mCastClientListener).setVerboseLoggingEnabled(true);
+
+            mApiClient = new GoogleApiClient.Builder(DemoMediaPlayerActivity.this)
+                    .addApi(Cast.API, apiOptionsBuilder.build())
+                    .addConnectionCallbacks(mConnectionCallbacks)
+                    .addOnConnectionFailedListener(mConnectionFailedListener)
+                    .build();
+
+            mApiClient.connect();
+        }
+
+        @Override
+        public void onRouteUnselected(MediaRouter router, MediaRouter.RouteInfo info) {
+            teardown();
+            mSelectedDevice = null;
+        }
+    }
+
+    private class ConnectionCallbacks implements
+            GoogleApiClient.ConnectionCallbacks {
+        @Override
+        public void onConnected(Bundle connectionHint) {
+            if (mWaitingForReconnect) {
+                mWaitingForReconnect = false;
+                Log.e(TAG, "Need to reconnect channels");
+                //reconnectChannels();
+            } else {
+                try {
+                    Cast.CastApi.launchApplication(mApiClient, CastMediaControlIntent.DEFAULT_MEDIA_RECEIVER_APPLICATION_ID, false)
+                            .setResultCallback(
+                                    new ResultCallback<Cast.ApplicationConnectionResult>() {
+                                        @Override
+                                        public void onResult(Cast.ApplicationConnectionResult result) {
+                                            Status status = result.getStatus();
+                                            if (status.isSuccess()) {
+                                                ApplicationMetadata applicationMetadata =
+                                                        result.getApplicationMetadata();
+                                                String sessionId = result.getSessionId();
+                                                String applicationStatus = result.getApplicationStatus();
+                                                boolean wasLaunched = result.getWasLaunched();
+
+                                                Log.d(TAG, "applicationMetadata: " + String.valueOf(applicationMetadata));
+                                                Log.d(TAG, "sessionId: " + sessionId);
+                                                Log.d(TAG, "applicationStatus: " + applicationStatus);
+                                                Log.d(TAG, "wasLaunched: " + String.valueOf(wasLaunched));
+
+                                                mApplicationStarted = true;
+
+                                                mRemoteMediaPlayer
+                                                        .requestStatus(mApiClient)
+                                                        .setResultCallback(
+                                                                new ResultCallback<RemoteMediaPlayer.MediaChannelResult>() {
+                                                                    @Override
+                                                                    public void onResult(RemoteMediaPlayer.MediaChannelResult result) {
+                                                                        if (!result.getStatus().isSuccess()) {
+                                                                            Log.e(TAG, "Failed to request status.");
+                                                                        }
+                                                                    }
+                                                                });
+
+                                                if (srgMediaPlayer.isPlaying()) {
+                                                    GoogleCastDelegate googleCatDelegate = new GoogleCastDelegate(mSessionId, mApiClient);
+                                                    srgMediaPlayer.setPlayerDelegateFactory();
+                                                }
+
+                                            } else {
+                                                teardown();
+                                            }
+                                        }
+                                    });
+
+                } catch (Exception e) {
+                    Log.e(TAG, "Failed to launch application", e);
+                }
+            }
+        }
+
+        @Override
+        public void onConnectionSuspended(int cause) {
+            mWaitingForReconnect = true;
+        }
+    }
+
+    private class ConnectionFailedListener implements
+            GoogleApiClient.OnConnectionFailedListener {
+        @Override
+        public void onConnectionFailed(ConnectionResult result) {
+            teardown();
+        }
+    }
+
+    Cast.Listener mCastClientListener = new Cast.Listener() {
+        @Override
+        public void onApplicationStatusChanged() {
+            if (mApiClient != null) {
+                Log.d(TAG, "onApplicationStatusChanged: "
+                        + Cast.CastApi.getApplicationStatus(mApiClient));
+            }
+        }
+
+        @Override
+        public void onVolumeChanged() {
+            if (mApiClient != null) {
+                Log.d(TAG, "onVolumeChanged: " + Cast.CastApi.getVolume(mApiClient));
+            }
+        }
+
+        @Override
+        public void onApplicationDisconnected(int errorCode) {
+            teardown();
+        }
+    };
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        int action = event.getAction();
+        int keyCode = event.getKeyCode();
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_VOLUME_UP:
+                if (action == KeyEvent.ACTION_DOWN) {
+                    if (mRemoteMediaPlayer != null) {
+                        double currentVolume = Cast.CastApi.getVolume(mApiClient);
+                        if (currentVolume < 1.0) {
+                            try {
+                                Cast.CastApi.setVolume(mApiClient,
+                                        Math.min(currentVolume + VOLUME_INCREMENT, 1.0));
+                            } catch (Exception e) {
+                                Log.e(TAG, "unable to set volume", e);
+                            }
+                        }
+                    } else {
+                        Log.e(TAG, "dispatchKeyEvent - volume up");
+                    }
+                }
+                return true;
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+                if (action == KeyEvent.ACTION_DOWN) {
+                    if (mRemoteMediaPlayer != null) {
+                        double currentVolume = Cast.CastApi.getVolume(mApiClient);
+                        if (currentVolume > 0.0) {
+                            try {
+                                Cast.CastApi.setVolume(mApiClient,
+                                        Math.max(currentVolume - VOLUME_INCREMENT, 0.0));
+                            } catch (Exception e) {
+                                Log.e(TAG, "unable to set volume", e);
+                            }
+                        }
+                    } else {
+                        Log.e(TAG, "dispatchKeyEvent - volume down");
+                    }
+                }
+                return true;
+            default:
+                return super.dispatchKeyEvent(event);
+        }
+    }
+
+    private void teardown() {
+        Log.d(TAG, "teardown");
+        if (mApiClient != null) {
+            if (mApplicationStarted) {
+                if (mApiClient.isConnected() || mApiClient.isConnecting()) {
+                    Cast.CastApi.stopApplication(mApiClient, mSessionId);
+                    mApiClient.disconnect();
+                }
+                mApplicationStarted = false;
+            }
+            mApiClient = null;
+        }
+        mSelectedDevice = null;
+        mWaitingForReconnect = false;
+        mSessionId = null;
+    }
+
 }
---
+++ srgmediaplayer/src/main/java/ch/srg/mediaplayer/internal/cast/GoogleCastDelegate.java
@@ -0,0 +1,7 @@
+package ch.srg.mediaplayer.internal.cast;
+
+/**
+ * Created by npietri on 19.10.15.
+ */
+public class GoogleCastDelegate {
+}
