004286a33ad9ff83b4b7aef48eef1df1fa38cba8
pietrini <nico.pietri@gmail.com>
2015-11-04 14:45:06 +0100

    inline Callback

--- srgmediaplayer-service/src/main/java/ch/srg/mediaplayer/internal/cast/ChromeCastManager.java
+++ srgmediaplayer-service/src/main/java/ch/srg/mediaplayer/internal/cast/ChromeCastManager.java
@@ -851,5 +851,28 @@ public class ChromeCastManager implements GoogleApiClient.ConnectionCallbacks, G
         }
     }
 
-}
+    public static class CastMediaRouterCallback extends MediaRouter.Callback{
+        private static final String TAG = "CastMediaRouterCallback";
 
+        private final ChromeCastManager chromeCastManager;
+
+        public CastMediaRouterCallback(ChromeCastManager chromeCastManager) {
+            this.chromeCastManager = chromeCastManager;
+        }
+
+        @Override
+        public void onRouteSelected(MediaRouter router, MediaRouter.RouteInfo info) {
+            Log.d(TAG, "onRouteSelected: info=" + info);
+            CastDevice device = CastDevice.getFromBundle(info.getExtras());
+            chromeCastManager.onDeviceSelected(device);
+            Log.d(TAG, "onRouteSelected: mSelectedDevice=" + device.getFriendlyName());
+        }
+
+        @Override
+        public void onRouteUnselected(MediaRouter router, MediaRouter.RouteInfo route) {
+            Log.d(TAG, "onRouteUnselected: route=" + route);
+            chromeCastManager.onDeviceSelected(null);
+        }
+
+    }
+}
