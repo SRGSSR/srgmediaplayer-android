4d22b1b7793894e05cc2e614b9f0514675609189
Sebastien Chauvin <sebastien.chauvin@gmail.com>
2016-02-08 15:53:24 +0100

    Pretty tab bar

--- demo/src/main/java/ch/srg/mediaplayer/LayoutsActivity.java
+++ demo/src/main/java/ch/srg/mediaplayer/LayoutsActivity.java
@@ -1,30 +1,37 @@
 package ch.srg.mediaplayer;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.LinearLayout;
 
 import ch.srg.mediaplayer.demo.R;
 
 public class LayoutsActivity extends AppCompatActivity {
-    private final int LAYOUT_LIST[];
+    private final int LAYOUT_IDS[];
+    private final String[] LAYOUT_NAMES;
 
     private ViewPager pager;
     private LayoutsPagerAdapter pagerAdapter;
+    private TabLayout tabLayout;
 
     public LayoutsActivity() {
-        LAYOUT_LIST = new int[]{
+        LAYOUT_IDS = new int[]{
                 R.layout.fragment_layout_one,
                 R.layout.fragment_layout_two,
                 R.layout.fragment_layout_three,
+                R.layout.fragment_layout_three_bis,
                 R.layout.fragment_layout_four
+        };
+        LAYOUT_NAMES = new String[]{
+                "width constraint",
+                "height constraint",
+                "vert. scroll w/o adjust",
+                "vert. scroll w/ adjust",
+                "hor. scrol w/ adjust"
         };
     }
 
@@ -37,21 +44,9 @@ public class LayoutsActivity extends AppCompatActivity {
 
         pagerAdapter = new LayoutsPagerAdapter(getSupportFragmentManager());
         pager.setAdapter(pagerAdapter);
-        ViewGroup buttons = (ViewGroup) findViewById(R.id.buttons);
 
-        for (int i = 0; i < LAYOUT_LIST.length; i++) {
-            Button button = new Button(this);
-            button.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-            button.setText(String.valueOf(i));
-            final int itemPosition = i;
-            button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    pager.setCurrentItem(itemPosition);
-                }
-            });
-            buttons.addView(button);
-        }
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(pager);
     }
 
     private class LayoutsPagerAdapter extends FragmentStatePagerAdapter {
@@ -63,14 +58,19 @@ public class LayoutsActivity extends AppCompatActivity {
         public Fragment getItem(int position) {
             LayoutFragment layoutFragment = new LayoutFragment();
             Bundle args = new Bundle();
-            args.putInt(LayoutFragment.ARG_LAYOUT_ID, LAYOUT_LIST[position]);
+            args.putInt(LayoutFragment.ARG_LAYOUT_ID, LAYOUT_IDS[position]);
             layoutFragment.setArguments(args);
             return layoutFragment;
         }
 
         @Override
+        public CharSequence getPageTitle(int position) {
+            return LAYOUT_NAMES[position];
+        }
+
+        @Override
         public int getCount() {
-            return LAYOUT_LIST.length;
+            return LAYOUT_IDS.length;
         }
     }
 }
