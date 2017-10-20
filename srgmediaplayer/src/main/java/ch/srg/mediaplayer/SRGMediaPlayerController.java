9166a007d51cb3ba5004b59c840df408126de938
Nicolas Pietri <nicolas.pietri@virtua.ch>
2017-10-20 14:21:04 +0200

    Remove MediaSession usage until we work more on this in the future

--- srgmediaplayer/src/main/java/ch/srg/mediaplayer/SRGMediaPlayerController.java
+++ srgmediaplayer/src/main/java/ch/srg/mediaplayer/SRGMediaPlayerController.java
@@ -416,8 +416,6 @@ public class SRGMediaPlayerController implements Handler.Callback,
     private ViewType viewType;
     private View renderingView;
     private Integer playbackState;
-    // TODO Use this in MediaSessionManager
-    private MediaSessionCompat mediaSession;
     private List<Segment> segments = new ArrayList<>();
 
     private Segment segmentBeingSkipped;
@@ -480,12 +478,6 @@ public class SRGMediaPlayerController implements Handler.Callback,
         exoPlayer.setAudioDebugListener(eventLogger);
         exoPlayer.setVideoDebugListener(eventLogger);
         exoPlayer.setMetadataOutput(eventLogger);
-
-        mediaSession = new MediaSessionCompat(this.context, "SRGMediaPlayerController");
-
-        MediaSessionConnector mediaSessionConnector =
-                new MediaSessionConnector(mediaSession);
-        mediaSessionConnector.setPlayer(exoPlayer, null);
     }
 
     private synchronized void startBackgroundThreadIfNecessary() {
