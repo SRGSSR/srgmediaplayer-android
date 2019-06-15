0afcdd1dbb4b4fe835ecea68f6a5b4945a79eaff
Sebastien Chauvin <sebastien.chauvin@rts.ch>
2019-06-15 10:11:49 +0200

    Fix

--- srgmediaplayer/src/main/java/ch/srg/mediaplayer/utils/MonitorTransferListener.java
+++ srgmediaplayer/src/main/java/ch/srg/mediaplayer/utils/MonitorTransferListener.java
@@ -1,5 +1,6 @@
 package ch.srg.mediaplayer.utils;
 
+import android.support.annotation.NonNull;
 import android.util.Log;
 import ch.srg.mediaplayer.SRGMediaPlayerController;
 import com.google.android.exoplayer2.upstream.DataSource;
@@ -12,24 +13,30 @@ import com.google.android.exoplayer2.upstream.TransferListener;
  * License information is available from the LICENSE file.
  */
 public class MonitorTransferListener implements TransferListener {
+    private TransferListener delegate;
+
+    public MonitorTransferListener(@NonNull TransferListener delegate) {
+        this.delegate = delegate;
+    }
+
     @Override
     public void onTransferInitializing(DataSource source, DataSpec dataSpec, boolean isNetwork) {
-        SRGMediaPlayerController.BANDWIDTH_METER.onTransferInitializing(source, dataSpec, isNetwork);
+        delegate.onTransferInitializing(source, dataSpec, isNetwork);
     }
 
     @Override
     public void onTransferStart(DataSource source, DataSpec dataSpec, boolean isNetwork) {
-        SRGMediaPlayerController.BANDWIDTH_METER.onTransferStart(source, dataSpec, isNetwork);
+        delegate.onTransferStart(source, dataSpec, isNetwork);
     }
 
     @Override
     public void onBytesTransferred(DataSource source, DataSpec dataSpec, boolean isNetwork, int bytesTransferred) {
-        SRGMediaPlayerController.BANDWIDTH_METER.onBytesTransferred(source, dataSpec, isNetwork, bytesTransferred);
+        delegate.onBytesTransferred(source, dataSpec, isNetwork, bytesTransferred);
     }
 
     @Override
     public void onTransferEnd(DataSource source, DataSpec dataSpec, boolean isNetwork) {
         Log.v(SRGMediaPlayerController.TAG, "DataSource: " + dataSpec.uri + " (" + dataSpec.length + ") ");
-        SRGMediaPlayerController.BANDWIDTH_METER.onTransferEnd(source, dataSpec, isNetwork);
+        delegate.onTransferEnd(source, dataSpec, isNetwork);
     }
 }
