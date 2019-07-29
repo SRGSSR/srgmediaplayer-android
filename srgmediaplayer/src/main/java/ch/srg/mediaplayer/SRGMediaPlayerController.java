7d9df0e9cc58cd8040342a5ca5243f191bf4ee2d
Joaquim Stähli <joaquim.stahli@rts.ch>
2019-07-29 11:28:33 +0200

    Code formatting

--- srgmediaplayer/src/main/java/ch/srg/mediaplayer/SRGMediaPlayerController.java
+++ srgmediaplayer/src/main/java/ch/srg/mediaplayer/SRGMediaPlayerController.java
@@ -6,11 +6,7 @@ import android.graphics.SurfaceTexture;
 import android.media.AudioManager;
 import android.media.MediaCodec;
 import android.net.Uri;
-import android.os.AsyncTask;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
-import android.os.SystemClock;
+import android.os.*;
 import android.support.v4.media.session.MediaSessionCompat;
 import android.text.TextUtils;
 import android.util.Log;
@@ -19,39 +15,18 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.TextureView;
 import android.view.View;
-
-import androidx.annotation.AnyThread;
-import androidx.annotation.IntDef;
-import androidx.annotation.MainThread;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
+import androidx.annotation.*;
+import ch.srg.mediaplayer.segment.model.Segment;
+import ch.srg.mediaplayer.utils.FileLicenseStore;
+import ch.srg.mediaplayer.utils.LicenseStoreDelegate;
+import ch.srg.mediaplayer.utils.MonitorTransferListener;
 import com.akamai.android.analytics.AkamaiMediaAnalytics;
 import com.akamai.android.analytics.EndReasonCodes;
 import com.akamai.android.analytics.PluginCallBacks;
-import com.google.android.exoplayer2.C;
-import com.google.android.exoplayer2.DefaultLoadControl;
-import com.google.android.exoplayer2.DefaultRenderersFactory;
-import com.google.android.exoplayer2.ExoPlaybackException;
-import com.google.android.exoplayer2.ExoPlayerFactory;
-import com.google.android.exoplayer2.Format;
-import com.google.android.exoplayer2.PlaybackParameters;
-import com.google.android.exoplayer2.Player;
-import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.*;
 import com.google.android.exoplayer2.audio.AudioCapabilities;
 import com.google.android.exoplayer2.audio.AudioCapabilitiesReceiver;
-import com.google.android.exoplayer2.drm.DefaultDrmSessionEventListener;
-import com.google.android.exoplayer2.drm.DefaultDrmSessionManager;
-import com.google.android.exoplayer2.drm.DrmInitData;
-import com.google.android.exoplayer2.drm.DrmSession;
-import com.google.android.exoplayer2.drm.ExoMediaDrm;
-import com.google.android.exoplayer2.drm.FrameworkMediaCrypto;
-import com.google.android.exoplayer2.drm.FrameworkMediaDrm;
-import com.google.android.exoplayer2.drm.HttpMediaDrmCallback;
-import com.google.android.exoplayer2.drm.MediaDrmCallback;
-import com.google.android.exoplayer2.drm.OfflineLicenseHelper;
-import com.google.android.exoplayer2.drm.UnsupportedDrmException;
+import com.google.android.exoplayer2.drm.*;
 import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.ProgressiveMediaSource;
@@ -64,20 +39,9 @@ import com.google.android.exoplayer2.source.hls.DefaultHlsDataSourceFactory;
 import com.google.android.exoplayer2.source.hls.HlsMediaSource;
 import com.google.android.exoplayer2.text.Cue;
 import com.google.android.exoplayer2.text.TextOutput;
-import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
-import com.google.android.exoplayer2.trackselection.MappingTrackSelector;
-import com.google.android.exoplayer2.trackselection.TrackSelection;
-import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.*;
 import com.google.android.exoplayer2.ui.spherical.SphericalSurfaceView;
-import com.google.android.exoplayer2.upstream.DataSource;
-import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
-import com.google.android.exoplayer2.upstream.DefaultHttpDataSource;
-import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
-import com.google.android.exoplayer2.upstream.FileDataSourceFactory;
-import com.google.android.exoplayer2.upstream.HttpDataSource;
-import com.google.android.exoplayer2.upstream.TransferListener;
+import com.google.android.exoplayer2.upstream.*;
 import com.google.android.exoplayer2.util.Util;
 import com.google.android.exoplayer2.video.VideoListener;
 
@@ -85,19 +49,7 @@ import java.io.IOException;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.ref.WeakReference;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.UUID;
-import java.util.WeakHashMap;
-
-import ch.srg.mediaplayer.segment.model.Segment;
-import ch.srg.mediaplayer.utils.FileLicenseStore;
-import ch.srg.mediaplayer.utils.LicenseStoreDelegate;
-import ch.srg.mediaplayer.utils.MonitorTransferListener;
+import java.util.*;
 
 /**
  * Handle the playback of media.
@@ -562,8 +514,9 @@ public class SRGMediaPlayerController implements Handler.Callback,
     private DrmConfig drmConfig;
     private int drmRequestDuration;
     private boolean drmRequestOffline;
-    private @SRGStreamType int currentStreamType;
-    private int numberOfDrmRetry=0;
+    private @SRGStreamType
+    int currentStreamType;
+    private int numberOfDrmRetry = 0;
 
     public static String getName() {
         return NAME;
@@ -783,7 +736,7 @@ public class SRGMediaPlayerController implements Handler.Callback,
         Long playbackStartPosition = startPositionMs;
         this.segments.clear();
         this.currentSegment = null;
-        currentStreamType=streamType;
+        currentStreamType = streamType;
         if (segments != null) {
             this.segments.addAll(segments);
             if (segment != null) {
@@ -927,7 +880,7 @@ public class SRGMediaPlayerController implements Handler.Callback,
                 return;
             }
             this.currentMediaUri = videoUri;
-            this.currentStreamType=streamType;
+            this.currentStreamType = streamType;
 
             DefaultHttpDataSourceFactory httpDataSourceFactory = new DefaultHttpDataSourceFactory(
                     userAgent,
@@ -1051,7 +1004,7 @@ public class SRGMediaPlayerController implements Handler.Callback,
      */
     private void doRelease() {
         if (this.state != State.RELEASED) {
-            numberOfDrmRetry=0;
+            numberOfDrmRetry = 0;
             if (mediaPlayerView != null) {
                 unbindFromMediaPlayerView(mediaPlayerView);
             }
@@ -2278,7 +2231,7 @@ public class SRGMediaPlayerController implements Handler.Callback,
                     });
                     break;
                 case Player.STATE_READY:
-                    numberOfDrmRetry=0;
+                    numberOfDrmRetry = 0;
                     if (!playingOrBuffering) {
                         broadcastEvent(Event.Type.MEDIA_READY_TO_PLAY);
                         playingOrBuffering = true;
